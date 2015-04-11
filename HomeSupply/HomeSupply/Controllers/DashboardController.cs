0e739e07feb08b1411f44a08a31253b2eef018db
leandro thiers <thiers.leandro@gmail.com>
2015-04-11 16:38:24 -0300

    Criando view e dashboard controller

---
+++ HomeSupply/HomeSupply/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeSupply.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+
+        public ActionResult Index()
+        {
+            ViewBag.Mensagem = "Olá mundo!";
+            return View();
+        }
+
+    }
+}
