7260d25265b80ab75726dafca6a84ee6a42c45fb
Fabricio Frontarolli <fabricio.frontarolli@gmail.com>
2015-04-11 14:40:23 -0300

    Initial Structure.

---
+++ HomeSupply/HomeSupply.Utils/SimplifyKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSupply.Utils
+{
+    public static class SimplifyKeys
+    {
+        public const string PublicKey = "sbpb_NDUyODg4ZDMtZjg3OS00YWVmLWFmYTEtMDNlZWVkNTk1YjNi";
+        public const string PrivateKey = "N7JO5db92fQJa/vS3yGCSrMeH7RBju4SEW45AMJdV7V5YFFQL0ODSXAOkNtXTToq";
+    }
+}
---
+++ HomeSupply/HomeSupply/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HomeSupply.MvcApplication" Language="C#" %>
