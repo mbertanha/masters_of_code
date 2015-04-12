800cb711e83ad66b1d833fad84b436134cbca4e4
Julio Oliveira <julio.contipelli@gmail.com>
2015-04-12 01:50:40 -0300

    Add materialize js,css and datepicker

--- js/catalog.js
+++ js/catalog.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
 	$('.recorrente').click(function () {
 		$('#frequencyGroup' + $(this).val()).toggle();                         
 	});    
+	
+	$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 });
