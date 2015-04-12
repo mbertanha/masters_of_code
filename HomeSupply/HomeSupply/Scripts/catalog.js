58f580a98c1581b28ac8ab748d281f13f7cbd985
Maurício Bertanha <mauriciobertanha@gmai.com>
2015-04-12 06:24:05 -0300

    Integrando site

---
+++ HomeSupply/HomeSupply/Scripts/catalog.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.recorrente').click(function () {
+		$('#frequencyGroup' + $(this).val()).toggle();                         
+	});    
+	
+	$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+});
