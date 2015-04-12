89c80f835154197959446b173edb81e642a79f96
Fabricio Frontarolli <fabricio.frontarolli@gmail.com>
2015-04-12 09:19:45 -0300

    Fixing Filter.

--- HomeSupply/HomeSupply/Scripts/catalog.js
+++ HomeSupply/HomeSupply/Scripts/catalog.js
@@ -6,15 +6,5 @@ $(document).ready(function () {
 	$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
-	});
-
-	if ($("#itemComprado").val()) {
-	    $("#" + $("#itemComprado").val()).fadeOut(2000, function()
-	    {
-	        $("#cat-" + $("#itemComprado").val()).show();
-	    });
-	    
-	    $("#sc_btn_" + $("#itemComprado").val()).hide();
-	    $("#check-" + $("#itemComprado").val()).show();
-	}
+  });
 });
--- HomeSupply/HomeSupply/Scripts/filter.js
+++ HomeSupply/HomeSupply/Scripts/filter.js
@@ -1,28 +1,109 @@
+// Get the array diff
+Array.prototype.diff = function (a) {
+    return this.filter(function (i) { return a.indexOf(i) < 0; });
+};
 
-    // Filter Items by Category Id
-    function applyFilterByCategory(categoryFilterId) {
-        $('.catalog-item[category-id=' + categoryFilterId + ']').show();
-    };
-    
-    // Expand Recurrence
-    function expandRecurrence() {
-        var recurrenceElementId = "#recurrenceElement";
-        $(recurrenceElementId).toggle('');
-    };
-        
-    $(document).ready(function () {
-    	    	    	
-        $('.category').click(function () {
-        	$('.catalog-item').hide();
-            var selectedCategories = $('.category:checked');
-            
-            if($(selectedCategories).length > 0){
-            	$(selectedCategories).each(function(){
-	            applyFilterByCategory($(this).val());
-	            });
-            }else{
-            	// None Selected - Show All
-            	$('.catalog-item').show();
-            }
-        });    
+
+// Filter Items by Category Id
+// Hide the itens that doest match this id
+function applyFilterByCategory(categoryFilterId) {
+    $('.catalog-item[category-id=' + categoryFilterId + ']')
+        .css('opacity', 0)
+        .slideDown('slow')
+        .animate(
+            { opacity: 1 },
+            { queue: false, duration: 'slow' }
+        );
+};
+
+function showItem(categoryFilterId) {
+    if (!$('.catalog-item[category-id=' + categoryFilterId + ']').is(':visible')) {
+        $('.catalog-item[category-id=' + categoryFilterId + ']')
+            .css('opacity', 0)
+            .slideDown('fast')
+            .animate(
+                { opacity: 1 },
+                { queue: false, duration: 'fast' }
+            );
+    }
+
+    if (!categoryFilterId) {
+        $('.catalog-item')
+            .css('opacity', 0)
+            .slideDown('fast')
+            .animate(
+                { opacity: 1 },
+                { queue: false, duration: 'fast' }
+            );
+    }
+}
+
+function showArray() {
+
+}
+
+function hideOthers(categoryFilterId) {
+    if ($('.catalog-item[category-id!=' + categoryFilterId + ']').length) {
+        if ($('.catalog-item[category-id!=' + categoryFilterId + ']').is(':visible')) {
+            $('.catalog-item[category-id!=' + categoryFilterId + ']')
+                .css('opacity', 1)
+                .slideUp('fast')
+                .animate(
+                    { opacity: 0 },
+                    { queue: false, duration: 'fast' }
+                );
+        }
+    }
+}
+
+function hideItem(categoryFilterId) {
+    if ($('.catalog-item[category-id=' + categoryFilterId + ']').length) {
+        if ($('.catalog-item[category-id=' + categoryFilterId + ']').is(':visible')) {
+            $('.catalog-item[category-id=' + categoryFilterId + ']')
+                .css('opacity', 1)
+                .slideUp('fast')
+                .animate(
+                    { opacity: 0 },
+                    { queue: false, duration: 'fast' }
+                );
+        }
+    }
+}
+
+// Expand Recurrence
+function expandRecurrence() {
+    var recurrenceElementId = "#recurrenceElement";
+    $(recurrenceElementId).toggle('');
+};
+
+$(document).ready(function () {
+
+    $('.category').click(function () {
+
+        var selectedId;
+
+        if ($('.category:checked').length === 0) {
+            showItem();
+        }
+        else if ($('.category:checked').length === 1) {
+            selectedId = $('.category:checked').val();
+
+            showItem(selectedId);
+            hideOthers(selectedId);
+        }
+        else {
+            var itens = $('.category').not($('.category:checked'));
+
+            console.log(itens);
+
+            $.each($('.category:checked'), function () {
+                showItem($(this).val());
+            });
+
+            $.each(itens, function () {
+                hideItem($(this).val());
+            });
+        }
+
     });
+});
