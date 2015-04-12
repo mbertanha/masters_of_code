eb506fdd06442efa43cf2b55822d7863d2f3bfba
Fabricio Frontarolli <fabricio.frontarolli@gmail.com>
2015-04-11 21:16:47 -0300

    jQuery - Filter products by CategoryId.

--- js/filter.js
+++ js/filter.js
@@ -1,32 +1,28 @@
 
     // Filter Items by Category Id
-    function applyFilter(productFilterId) {
-    
-        // Hide products that doesn't have this data-attr Product-ID
-    
-        $('#produtos[product-id!=' + productFilterId + ']').hide();
-        $('#produtos[product-id=' + productFilterId + ']').show();
-        
-    
+    function applyFilterByCategory(categoryFilterId) {
+        $('.catalog-item[category-id=' + categoryFilterId + ']').show();    
     };
     
     // Expand Recurrence
     function expandRecurrence() {
         var recurrenceElementId = "#recurrenceElement";
-    
         $(recurrenceElementId).toggle('');
     };
-    
-    
-    $(document).load(function () {
-    
-        var elementFilter = '.category';
-    
-        $(elementFilter).find('input').click(index, function () {
-                
-            var productId = $(this).val();
-            applyFilter(productId);
-    
-        });
-    
+        
+    $(document).ready(function () {
+    	    	    	
+        $('.category').click(function () {
+        	$('.catalog-item').hide();
+            var selectedCategories = $('.category:checked');
+            
+            if($(selectedCategories).length > 0){
+            	$(selectedCategories).each(function(){
+	            applyFilterByCategory($(this).val());
+	            });
+            }else{
+            	// None Selected - Show All
+            	$('.catalog-item').show();
+            }
+        });    
     });
