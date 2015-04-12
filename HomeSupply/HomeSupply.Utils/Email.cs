a6e56d6d11f25f02a3d8cb2fd676e7f85293de0b
leandro thiers <thiers.leandro@gmail.com>
2015-04-12 01:57:17 -0300

    Adicionando classe de envio de e-mail

---
+++ HomeSupply/HomeSupply.Utils/Email.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace HomeSupply.Utils
+{
+    public class Email
+    {
+
+        public Boolean enviarEmail()
+        {
+            SmtpClient smtp = new SmtpClient();
+            MailMessage email = new MailMessage();
+
+            try
+            {
+                //Instanciar objeto smtp
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential("samsungs3da@gmail.com", "samsung.");
+                
+
+                email.Sender = new MailAddress("jukaroxteam@gmail.com", "Jukarox");
+                email.From = new MailAddress("jukaroxteam@gmail.com", "Jukarox");
+                email.To.Add(new MailAddress("jukarox@gmail.com", "Juquinha"));
+                email.Subject = "Notificação de pagamento!";
+                email.Body = "Seu pagamento foi efetuado com sucesso.";
+                email.IsBodyHtml = false;
+                email.Priority = MailPriority.High;
+
+                smtp.Send(email);
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
