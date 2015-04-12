9f1d9ffa707a5077d7a264d08da5b4625dc5da54
Maurício Bertanha <mauriciobertanha@gmai.com>
2015-04-12 00:21:53 -0300

    Recurring Payments

--- HomeSupply/HomeSupply/Controllers/DashboardController.cs
+++ HomeSupply/HomeSupply/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using HomeSupply.Utils;
 using SimplifyCommerce.Payments;
@@ -7,12 +8,11 @@ namespace HomeSupply.Controllers
 {
     public class DashboardController : Controller
     {
-
-        public ActionResult Index(string cardToken, string amount)
+        public ActionResult Index(string cardToken, string amount, string name, string email, string reference)
         {
-            if (!String.IsNullOrEmpty(cardToken) && !String.IsNullOrEmpty(amount))
+            if (!String.IsNullOrEmpty(cardToken) && !String.IsNullOrEmpty(amount) && !String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(email))
             {
-                if (Pagamento(cardToken, amount))
+                if (Pagamento(cardToken, amount, name, email, reference))
                 {
                     ViewBag.Mensagem = "Pagamento Realizado com Sucesso!";
                 }
@@ -24,28 +24,69 @@ namespace HomeSupply.Controllers
             
             return View();
         }
-        public bool Pagamento(string token, string amount)
+        public bool Pagamento(string token, string amount, string name, string email, string reference)
         {
             PaymentsApi.PrivateApiKey = SimplifyKeys.PrivateKey;
             PaymentsApi.PublicApiKey = SimplifyKeys.PublicKey;
 
             PaymentsApi api = new PaymentsApi();
 
-            Payment payment = new Payment();
-            payment.Amount = Convert.ToInt64(amount);
-            payment.Currency = CurrencyInfo.Currency;
-            payment.Description = CurrencyInfo.DescriptionPayment;
-            payment.Token = token;
-
-            try
+            // Pontual
+            if (String.IsNullOrEmpty(reference))
             {
-                payment = (Payment)api.Create(payment);
+                Payment payment = new Payment();
+                payment.Amount = Convert.ToInt64(amount);
+                payment.Currency = CurrencyInfo.Currency;
+                payment.Description = CurrencyInfo.DescriptionPayment;
+                payment.Token = token;
 
-                return payment.PaymentStatus.Equals("APPROVED");
+                try
+                {
+                    payment = (Payment) api.Create(payment);
+
+                    return payment.PaymentStatus.Equals("APPROVED");
+                }
+                catch
+                {
+                    return false;
+                }
             }
-            catch
+            else
             {
-                return false;
+                Plan plan = new Plan();
+                plan.Amount = Convert.ToInt64(amount);
+                plan.Frequency = reference;
+                plan.Name = "Recurring Payment";
+
+                try
+                {
+                    plan = (Plan)api.Create(plan);
+                }
+                catch
+                {
+                    return false;
+                }
+
+                Customer customer = new Customer();
+                customer.Token = token;
+                customer.Email = email;
+                customer.Name = name;
+                List<Subscription> subscriptions = new List<Subscription>();
+                Subscription subscription = new Subscription();
+                subscription.Plan = plan;
+                subscriptions.Add(subscription);
+                customer.Subscriptions = subscriptions;
+
+                try
+                {
+                    api.Create(customer);
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+
+                return true;   
             }
         }
 
